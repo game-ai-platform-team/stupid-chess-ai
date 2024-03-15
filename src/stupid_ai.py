b8bebee2181eda548b1fbf85f090e18581592fc0
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-03-15 13:22:43 +0200

    feat: Add START tag for new game

--- src/stupid_ai.py
+++ src/stupid_ai.py
@@ -6,6 +6,13 @@ import random
 def set_board(board: chess.Board, board_position:str):
     board.set_fen(board_position)
 
+def make_move(board: chess.Board):
+    legal_moves = [move.uci() for move in list(board.legal_moves)]
+    choice = random.choice(legal_moves)
+    board.push_uci(choice)
+
+    return choice
+
 def main():
 
     board = chess.Board()
@@ -15,11 +22,14 @@ def main():
         time.sleep(random.randrange(1,10)/100)
         if opponent_move.startswith("BOARD: "):
             set_board(board, opponent_move.removeprefix("BOARD: "))
+        elif opponent_move.startswith("START: "):
+            choice = make_move(board)
+            print("Started new game!")
+            print(f"MOVE: {choice}")
         elif opponent_move.startswith("MOVE: "):
             board.push_uci(opponent_move.removeprefix("MOVE: "))
-            legal_moves = [move.uci() for move in list(board.legal_moves)]
-            choice = random.choice(legal_moves)
-            board.push_uci(choice)
+            choice = make_move(board)
+
             # example about logs
             print(f"I moved {choice}\n")
             # example about posting a move
