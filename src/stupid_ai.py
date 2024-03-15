01d5c9de741f0f2cf23da776152b3b1abaade511
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-03-15 12:39:59 +0200

    fix: Revert back to local compatible stupid chess AI

--- src/stupid_ai.py
+++ src/stupid_ai.py
@@ -3,6 +3,9 @@ import chess
 import time
 import random
 
+def set_board(board: chess.Board, board_position:str):
+    board.set_fen(board_position)
+
 def main():
 
     board = chess.Board()
@@ -10,16 +13,17 @@ def main():
     while True:
         opponent_move = input()
         time.sleep(random.randrange(1,10)/100)
-        if opponent_move != "":
-            board.push_uci(opponent_move)
-        legal_moves = [move.uci() for move in list(board.legal_moves)]
-        choice = random.choice(legal_moves)
-        board.push_uci(choice)
-
-        # example about logs
-        print(f"I moved {choice}\n")
-        # example about posting a move
-        print(f"MOVE: {choice}\n")                                                                                                              
+        if opponent_move.startswith("BOARD: "):
+            set_board(board, opponent_move.removeprefix("BOARD: "))
+        elif opponent_move.startswith("MOVE: "):
+            board.push_uci(opponent_move.removeprefix("MOVE: "))
+            legal_moves = [move.uci() for move in list(board.legal_moves)]
+            choice = random.choice(legal_moves)
+            board.push_uci(choice)
+            # example about logs
+            print(f"I moved {choice}\n")
+            # example about posting a move
+            print(f"MOVE: {choice}\n")   
 
 if __name__ == "__main__":
     main()
